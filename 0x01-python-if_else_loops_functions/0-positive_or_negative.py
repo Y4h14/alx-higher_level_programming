125070bb579c3bad129a95b873fb65d6dd4fa279
Y4h14 <yahyasadig2013@gmail.com>
2023-10-31 23:20:12 +0300

    !

--- 0x01-python-if_else_loops_functions/0-positive_or_negative.py
+++ 0x01-python-if_else_loops_functions/0-positive_or_negative.py
@@ -3,7 +3,7 @@ import random
 number = random.randint(-10, 10)
 if number == 0:
     print(f"{number} is zero")
-elif number % 2 == 0:
-    print(f"{number} is even")
+elif number < 0:
+    print(f"{number} is negative")
 else:
-    print(f"{number} is odd")
+    print(f"{number} is positive")
--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -11,4 +11,5 @@ elif number % x < 6:
     if number % x == 0:
         print(f"Last digit of {number} is {number % x} and is 0")
     else:
-        print(f"Last digit of {number} is {number % x} and is less than 6 and not 0")
+        print(f"Last digit of {number} is {number % x} and is\
+                less than 6 and not 0")
