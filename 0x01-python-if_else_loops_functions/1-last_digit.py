653bb4580254050ffe20147dd785669d9c5622a5
Y4h14 <yahyasadig2013@gmail.com>
2023-10-31 23:02:35 +0300

    initial commit

---
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+import random
+number = random.randint(-10000, 10000)
+if number < 0:
+    x = -10
+else:
+    x = 10
+if number % x > 5:
+    print(f"Last digit of {number} is {number % x} and is greater than 5")
+elif number % x < 6:
+    if number % x == 0:
+        print(f"Last digit of {number} is {number % x} and is 0")
+    else:
+        print(f"Last digit of {number} is {number % x} and is less than 6 and not 0")
---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def uppercase(str):
+    result = ""
+    for c in str:
+        if ord(c) in range(97, 123):
+            upper = ord(c) - 32
+            result += chr(upper) 
+        else:
+            result += c
+    print(result)
