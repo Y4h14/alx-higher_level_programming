e4051986f4e4683a674353d8ebe858d76a679f07
Y4h14 <yahyasadig2013@gmail.com>
2023-11-01 21:38:33 +0300

    the c task

--- 0x00-python-hello_world/lists.h
+++ 0x00-python-hello_world/lists.h
@@ -1,19 +1,25 @@
 #ifndef LISTS_H
 #define LISTS_H
-#include <stddef.h>
-typedef struct listint_t *nodeptr;
-/**
-* struct listint_t - a linked list implementation
-* @n: the value of the node (int)
-* @next: pointer to the next node
-*/
-struct listint_t
-{
-	int n;
-	nodeptr next;
-};
-typedef struct listint_t listint_t;
 
+#include <stdlib.h>
+
+/**
+ * struct listint_s - singly linked list
+ * @n: integer
+ * @next: points to the next node
+ *
+ * Description: singly linked list node structure
+ * 
+ */
+typedef struct listint_s
+{
+    int n;
+    struct listint_s *next;
+} listint_t;
+
+size_t print_listint(const listint_t *h);
+listint_t *add_nodeint(listint_t **head, const int n);
+void free_listint(listint_t *head);
 int check_cycle(listint_t *list);
 
-#endif
+#endif /* LISTS_H */
