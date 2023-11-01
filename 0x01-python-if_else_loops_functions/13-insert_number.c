e89d707c3cf6a6e36338365bf589dabee1d8a03e
Y4h14 <yahyasadig2013@gmail.com>
2023-11-01 21:33:42 +0300

    the c task

---
+++ 0x01-python-if_else_loops_functions/13-insert_number.c
@@ -0,0 +1,30 @@
+#include "lists.h"
+/**
+ * insert_node - insters a number into a sorted linked list
+ * @head: pointer to the head of the list
+ * @number: the number to be added
+ * Return: a pointer to the new node or NULL
+ */
+listint_t *insert_node(listint_t **head, int number)
+{
+	listint_t *node = *head, *new;
+
+	new = malloc(sizeof(listint_t));
+	if (new == NULL)
+		return (NULL);
+	new->n = number;
+
+	if (node == NULL || node->n >= number)
+	{
+		new->next = node;
+		*head = new;
+		return (new);
+	}
+	while (node && node->next && node->next->n < number)
+		node = node->next;
+
+	new->next = node->next;
+	node->next = new;
+
+	return (new);
+}
---
+++ 0x01-python-if_else_loops_functions/lists.h
@@ -0,0 +1,18 @@
+#ifndef LISTS_H
+#define LISTS_H
+#include <stdlib.h>
+/**
+ * struct listint_s - singly linked list
+ * @n : intiger
+ * @next: point to the next node
+ */
+typedef struct listint_s
+{
+	int n;
+	struct listint_s *next;
+} listint_t;
+
+listint_t *insert_node(listint_t **head, int number);
+
+
+#endif /* LISTS_H */
