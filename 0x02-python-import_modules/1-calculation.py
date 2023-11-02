b8b95381baae034dcb4ffb4783f803ef982ad4dc
Y4h14 <yahyasadig2013@gmail.com>
2023-11-02 22:40:02 +0300

    !

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
-if __name__ == "__main__":
-    """ print calculations on 10 and 5 """
+if __name__ != "__main__":
+    exit()
 from calculator_1 import add, sub, mul, div
 a = 10
 b = 5
