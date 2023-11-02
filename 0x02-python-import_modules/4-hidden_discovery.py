86c35f10d5473740382137789908ea956ea92c4c
Y4h14 <yahyasadig2013@gmail.com>
2023-11-02 20:30:03 +0300

    !

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -2,7 +2,7 @@
 if __name__ == "__main__":
     """print names defined in the module"""
 import hidden_4
-module_naime = dir(hidden_4)
+module_name = dir(hidden_4)
 for module in module_name:
     if module[:2] != "__":
         print(module)
