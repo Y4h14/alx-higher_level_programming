51da3dfe8238204f1d3af579e3a598a14419bcad
Y4h14 <yahyasadig2013@gmail.com>
2023-11-22 16:27:33 +0300

    task 6 fix

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -34,10 +34,11 @@ class Square:
         temp = self.__position[1]
         if self.__size == 0:
             print()
-        for i in range(self.__position[1]):
-            print()
-        for j in range(self.__size):
-            print(" " * self.__position[0] + "#" * self.__size)
+        else:
+            for i in range(self.__position[1]):
+                print()
+            for j in range(self.__size):
+                print(" " * self.__position[0] + "#" * self.__size)
 
     @property
     def size(self):
