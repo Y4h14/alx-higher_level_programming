7a2c90859979ef3016af89b133df7167a3544afc
Y4h14 <yahyasadig2013@gmail.com>
2023-11-27 18:50:46 +0300

    updated __str__ (task 3)

--- 0x08-python-more_classes/3-rectangle.py
+++ 0x08-python-more_classes/3-rectangle.py
@@ -55,7 +55,9 @@ class Rectangle:
     def __str__(self):
         result = ""
         for i in range(self.__height):
-            result += "#" * self.__width + '\n'
+            result += "#" * self.__width
+            if i != self.height - 1:
+                result += '\n'
         return (result)
 
     def area(self):
