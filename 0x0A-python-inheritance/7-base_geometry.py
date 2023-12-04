739ba0645d7935a20db33216f4719710373e6972
Y4h14 <yahyasadig2013@gmail.com>
2023-12-04 21:31:13 +0300

    !

--- 0x0A-python-inheritance/7-base_geometry.py
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -15,7 +15,7 @@ class BaseGeometry:
             -name (str): a string for a name
             -value (int): the value
         """
-        if not isinstance(value, int):
+        if type(value) != int:
             raise TypeError(name + ' must be an integer')
         if value <= 0:
             raise ValueError(name + ' must be greater than 0')
