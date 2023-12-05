dcd09554e97f4fecda3b311301af21af99faaec7
Y4h14 <yahyasadig2013@gmail.com>
2023-12-05 16:22:02 +0300

    added file doc

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
-
+"""defines a function that reads a file and print it's content"""
 
 def read_file(filename=""):
+    """reads contents of a file and print them"""
     with open(filename, encoding="utf-8") as file:
         print(file.read(), end='')
