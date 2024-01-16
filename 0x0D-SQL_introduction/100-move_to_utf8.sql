4e9caa690bde1a0e562f5a7ae3e344f9f4eaf459
y4h14 <yahyasadig2013@gmail.com>
2024-01-17 00:51:09 +0300

    fixed a typo

--- 0x0D-SQL_introduction/100-move_to_utf8.sql
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -1,3 +1,6 @@
 -- converts the database to utf-8
+-- specify the database
+USE hbtn_0c_0;
+-- converting to utf8
 ALTER TABLE `first_table`
-CONVERT TO CHARACTAR SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
