ade19e6191b83e06a1534d68d45a7a0536db51ea
Y4h14 <yahyasadig2013@gmail.com>
2024-02-12 23:17:44 +0300

    updated task 11

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -7,12 +7,7 @@ while (args[i] !== undefined) {
   if (args[i] > max) {
     semiMax = max;
     max = parseInt(args[i]);
-  } else if (args[i] > semiMax) {
-    semiMax = parseInt(args[i]);
   }
   i++;
-}
-if (args[3] === undefined) {
-  semiMax = 0;
 }
 console.log(semiMax);
