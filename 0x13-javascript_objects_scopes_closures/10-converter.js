fc76abf8179b30613215c840bdf34ca7f9fd8435
Y4h14 <yahyasadig2013@gmail.com>
2024-02-13 18:11:12 +0300

    modified tasks 9 and 10

--- 0x13-javascript_objects_scopes_closures/10-converter.js
+++ 0x13-javascript_objects_scopes_closures/10-converter.js
@@ -1,7 +1,6 @@
 #!/usr/bin/node
 exports.converter = function (base) {
-  const setBase = base;
   return function (num) {
-    return num.toString(setBase);
+    return num.toString(base);
   };
 };
