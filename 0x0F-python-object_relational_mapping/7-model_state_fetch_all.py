f95882cf88110d6ca6552cb0e89630538d8dc12d
y4h14 <yahyasadig2013@gmail.com>
2024-02-18 23:35:51 +0300

    task 7

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
+"""
+Start link class to table in database
+"""
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from model_state import Base, State
