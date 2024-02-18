fa9991b377e9b2976d2b3c7914bcedfd82012c78
y4h14 <yahyasadig2013@gmail.com>
2024-02-18 22:28:10 +0300

    model_state

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -2,19 +2,14 @@
 """defines state class and an instanse of sqlAlchemy Base"""
 from sqlalchemy import create_engine, Column, String, Integer
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker
-
-engine = create_engine('mysql://\'user1\':\'Pass123\'}@localhost:3306/hbtn_0d_usa', echo=True)
-
 
 Base = declarative_base()
 
 
 class State(Base):
+    """
+    Class for the states table
+    """
     __tablename__ = 'states'
     id = Column(Integer, primary_key=True, autoincrement=True)
     name = Column(String(128), nullable=False)
-
-
-
-Base.metadata.create_all(bind=engine)
