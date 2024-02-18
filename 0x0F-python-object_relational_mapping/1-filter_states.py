a27817ea16472f41d3cf5ceed6267fb7d871dfb9
y4h14 <yahyasadig2013@gmail.com>
2024-02-18 17:30:43 +0300

    task1

---
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python3
+"""list all rows form the states table"""
+import MySQLdb
+import sys
+
+
+def states_list(username, passwrod, database):
+    """list all rows from the states table
+        Args:
+        username: the username for the database
+        passwrod: the password for the database
+        db_name: the name of the database
+    """
+    db = MySQLdb.connect(
+        host='localhost',
+        port=3306,
+        user=username,
+        passwd=passwrod,
+        db=database)
+    cur = db.cursor()
+
+    query = "SELECT * FROM states WHERE  name Like 'N%' ORDER BY id ASC"
+    cur.execute(query)
+
+    rows = cur.fetchall()
+
+    for row in rows:
+        print(row)
+
+    cur.close()
+    db.close()
+
+
+if __name__ == "__main__":
+    if len(sys.argv) == 4:
+        username, password, database = sys.argv[1:]
+        states_list(username, password, database)
