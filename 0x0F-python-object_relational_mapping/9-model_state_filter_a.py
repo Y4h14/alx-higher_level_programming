f46e39073d9aa60aab32a491f25b7a56aeebdec5
y4h14 <yahyasadig2013@gmail.com>
2024-02-18 23:22:37 +0300

    task 9

--- 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
+++ 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
@@ -8,7 +8,7 @@ import sys
 if __name__ == "__main__":
 
     engine = create_engine(
-        f"mysql+mysqldb://{sys.argv[1]}:{sys.argv[2]}@localhost/{sys.argv[3]}",
+        f"mysql+mysqldb://{sys.argv[1]}:{sys.argv[2]}@localhost:3306/{sys.argv[3]}",
         pool_pre_ping=True)
 
     Session = sessionmaker(bind=engine)
