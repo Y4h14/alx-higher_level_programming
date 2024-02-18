32e5d7b95d0de7f25d8887ceaec4254198b5662c
y4h14 <yahyasadig2013@gmail.com>
2024-02-19 01:10:30 +0300

    tasks 10 -13

--- 0x0F-python-object_relational_mapping/10-model_state_my_get.py
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -5,10 +5,11 @@ Start link class to table in database
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from model_state import State, Base
+
 import sys
 
-
 if __name__ == "__main__":
+
     engine = create_engine(
         "mysql+mysqldb://{}:{}@localhost:3306/{}"
         .format(sys.argv[1], sys.argv[2], sys.argv[3]))
@@ -17,8 +18,8 @@ if __name__ == "__main__":
     Session = sessionmaker(bind=engine)
     session = Session()
 
-    result = session.query(State).first()
-    if result == None:
-        print('Nothing')
-    else:
-        print(f'{result.id}: {result.name}')
+    result = session.query(State).filter(State.name == (sys.argv[4],))
+    try:
+        print(result[0].id)
+    except IndexError:
+        print('Not found')
--- 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
+++ 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
@@ -5,10 +5,11 @@ Start link class to table in database
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from model_state import State, Base
+
 import sys
 
-
 if __name__ == "__main__":
+
     engine = create_engine(
         "mysql+mysqldb://{}:{}@localhost:3306/{}"
         .format(sys.argv[1], sys.argv[2], sys.argv[3]))
@@ -17,8 +18,7 @@ if __name__ == "__main__":
     Session = sessionmaker(bind=engine)
     session = Session()
 
-    result = session.query(State).first()
-    if result == None:
-        print('Nothing')
-    else:
-        print(f'{result.id}: {result.name}')
+    states = session.query(State).filter(State.name.like('%a%')).all()
+    for state in states:
+        session.delete(state)
+    session.commit()
