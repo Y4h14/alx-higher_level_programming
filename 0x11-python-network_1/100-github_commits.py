586107756fca654617925f9401ee679b3d84cbd0
Y4h14 <yahyasadig2013@gmail.com>
2024-03-06 00:29:29 +0300

    updated requirement for task 100

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -3,16 +3,16 @@
 from sys import argv
 import requests
 
-
-repo = argv[1]
-owner = argv[2]
-url = f"https://api.github.com/repos/{owner}/{repo}/commits"
-headers = {'Accept': 'application/vnd.github+json',
-           'X-GitHub-Api-Version': '2022-11-28'}
-res = requests.get(url, headers=headers)
-try:
-    commits = res.json()[:10]
-except Exception:
-    user_id = None
-for commit in commits:
-    print(f"{commit['sha']}: {commit['commit']['author']['name']}")
+if __name__ == '__main__':
+    repo = argv[1]
+    owner = argv[2]
+    url = f"https://api.github.com/repos/{owner}/{repo}/commits"
+    headers = {'Accept': 'application/vnd.github+json',
+               'X-GitHub-Api-Version': '2022-11-28'}
+    res = requests.get(url, headers=headers)
+    try:
+        commits = res.json()[:10]
+    except Exception:
+        commits = None
+    for commit in commits:
+        print(f"{commit['sha']}: {commit['commit']['author']['name']}")
